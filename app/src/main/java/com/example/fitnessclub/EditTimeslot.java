3e27aa0b32af1a842f9557b38bd47ee682a4a59d
Usama Zafar <osamazafar97@gmail.com>
2020-10-20 13:00:54 +0500

    EditTimeslot Page 3/4 done

---
+++ app/src/main/java/com/example/fitnessclub/EditTimeslot.java
@@ -0,0 +1,14 @@
+package com.example.fitnessclub;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EditTimeslot extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_timeslot);
+    }
+}
